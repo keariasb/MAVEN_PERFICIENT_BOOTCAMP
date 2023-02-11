097390d44ee1e9da4cf1c2629328d0e186003680
Kvnrs <keariasb@unal.edu.co>
2023-02-11 16:31:41 -0500

    primer proyecto
    XDD

---
+++ src/main/java/com/example/MAVEN_PERFICIENT_BOOTCAMP/MavenPerficientBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.example.MAVEN_PERFICIENT_BOOTCAMP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPerficientBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPerficientBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MAVEN_PERFICIENT_BOOTCAMP/MavenPerficientBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MAVEN_PERFICIENT_BOOTCAMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPerficientBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
